fc5a25db6a1702ca6d2d6f1300a22ba3f6af922c
Boyman10 <contact@yasi.fr>
2018-02-25 16:31:39 +0100

    Adding register form using Struts 2

---
+++ ouhai-business/src/main/java/org/ouhai/demo/business/action/HelloWorldAction.java
@@ -0,0 +1,44 @@
+package org.ouhai.demo.business.action;
+
+import org.ouhai.demo.business.model.MessageStore;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloWorldAction extends ActionSupport {
+    private MessageStore messageStore;
+    
+    private static int helloCount = 0;
+    
+    private String userName;
+
+    
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+    
+    public int getHelloCount() {
+        return helloCount;
+    }
+    
+    
+    public String execute() {
+    	   	
+        messageStore = new MessageStore() ;
+        
+    	if (userName != null) {
+    	    messageStore.setMessage( messageStore.getMessage() + " " + userName);
+    	}
+    	
+    	
+        helloCount++;
+        return SUCCESS;
+    }
+
+    public MessageStore getMessageStore() {
+        return messageStore;
+    }
+}
---
+++ ouhai-business/src/main/java/org/ouhai/demo/business/model/MessageStore.java
@@ -0,0 +1,20 @@
+package org.ouhai.demo.business.model;
+
+public class MessageStore {
+    private String message;
+    
+    public MessageStore() {
+        message = "Hello Struts User";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
+    }   
+    @Override
+    public String toString() {
+        return message + " (from toString)";
+    }    
+}
